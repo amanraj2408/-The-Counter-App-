df6ae19fc0762fa8b81b0dbea73a2a29adbdaf78
Aman Raj <amanraj5687@gmail.com>
2025-06-20 15:15:24 +0530

    Update App to Counter App and clean unused files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import React from 'react';
 
 function App() {
+
+  const [num, setNum] = useState(0); // when we want to show the changes in the UI, we need to use useState hook
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <h2>Count {num}</h2>
+      <button onClick={() => setNum(num + 1)}>Increment</button>
+      <button onClick={() => setNum(num - 1)}>Decrement</button>
+      
     </div>
   );
 }
